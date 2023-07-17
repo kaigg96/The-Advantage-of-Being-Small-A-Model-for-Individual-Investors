a2eb29d9aa73f323e97406d51e5a2902de300fd0
Kai Groden-Gilchrist <115024722+kaigg96@users.noreply.github.com>
2023-07-16 18:42:33 -0700

    Update app.py to revert all to h5s

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ app.config['SECRET_KEY'] = 'asecretkey'
 # 3. define a prediction function
 # 3a. load data
 small_cap_price_data = pd.read_hdf('small_cap_price_data.h5', key='small_cap_price_data')
-closing_price_data = pd.read_pickle('closing_price_data.pkl')
+closing_price_data = pd.read_hdf('closing_price_data.h5', key='closing_price_data')
+high_price_data = pd.read_hdf('high_price_data.h5', key='high_price_data')
+low_price_data = pd.read_hdf('low_price_data.h5', key='low_price_data')
 
 def min_var_filter(price_data, model_start_date, var=0.1, period=200):
     
